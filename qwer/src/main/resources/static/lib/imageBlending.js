c2b304436b0d6a915551041c5dffb54eb6e94a67
yonghunJ <rrallvv@gmail.com>
2019-04-17 23:08:04 -0400

    ADD libraries for images blening

---
+++ qwer/src/main/resources/static/lib/imageBlending.js
@@ -0,0 +1,34 @@
+
+
+var image_set = {base:null, src:null};
+
+function changeImageSet() {
+    var base = document.getElementById("_mask");
+    var image = document.getElementById("_img");
+    image_set.base = base.src;
+    image_set.src = image.src;
+    initializeCanvas();
+}
+function changeImageSet2() {
+    var base = document.getElementById("_hand");
+    var image = document.getElementById("_sign");
+    image_set.base = base.src;
+    image_set.src = image.src;
+    initializeCanvas();
+}
+
+window.onload = function() {
+    base_canvas = document.getElementById("base-canvas");
+    src_canvas = document.getElementById("src-canvas");
+    mask_canvas = document.getElementById("mask-canvas");
+    result_canvas = document.getElementById("result-canvas");
+
+    base_ctx = base_canvas.getContext("2d");
+    src_ctx = src_canvas.getContext("2d");
+    mask_ctx = mask_canvas.getContext("2d");
+    result_ctx = result_canvas.getContext("2d");
+
+    window.onscroll = function(){ calMaskCanvasOffset(); };
+
+    initializeCanvas();
+};
