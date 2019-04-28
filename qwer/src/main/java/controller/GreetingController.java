d375c7d192bcb3b525a112409fee15604db97dfe
yonghunJ <rrallvv@gmail.com>
2019-04-28 11:03:21 -0400

    ADD server side socket and bean

---
+++ qwer/src/main/java/controller/GreetingController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import model.Greeting;
+import model.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.HtmlUtils;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+    @RequestMapping("/hallo")
+    public String login(Map<String, Object> model) {
+        model.put("message", "You are in new page !!");
+        return "index1";
+    }
+}
