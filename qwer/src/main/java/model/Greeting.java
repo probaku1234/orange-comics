d8f23ceb4d854f892dfa7a267704b2f5c74ebb3b
yonghunJ <rrallvv@gmail.com>
2019-05-03 09:50:08 -0400

    ADD class for message' s information

--- qwer/src/main/java/model/Greeting.java
+++ qwer/src/main/java/model/Greeting.java
@@ -3,14 +3,42 @@ package model;
 public class Greeting {
 
     private String content;
+    private String sender;
+    private String receiver;
 
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
     public Greeting() {
     }
 
     public Greeting(String content) {
+
         this.content = content;
     }
+    public Greeting(String sender,String receiver,String content) {
 
+        this.content = content;
+        this.sender = sender;
+        this.receiver = receiver;
+
+    }
     public String getContent() {
         return content;
     }
