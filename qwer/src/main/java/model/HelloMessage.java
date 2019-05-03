d5f89ad19d14a4a84de2ecefc63a2ba09edab548
yonghunJ <rrallvv@gmail.com>
2019-05-03 09:51:02 -0400

    ADD Class to check whether messanger works or not

--- qwer/src/main/java/model/HelloMessage.java
+++ qwer/src/main/java/model/HelloMessage.java
@@ -3,6 +3,25 @@ package model;
 public class HelloMessage {
 
     private String name;
+    private String user2;
+
+    public String getUser2() {
+        return user2;
+    }
+
+    public void setUser2(String user2) {
+        this.user2 = user2;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private String message;
 
     public HelloMessage() {
     }
