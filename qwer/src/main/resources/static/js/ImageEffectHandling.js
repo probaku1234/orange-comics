b9b91adeeb7fe135f6e034ba562b7d05cdd74c3f
yonghunJ <rrallvv@gmail.com>
2019-05-21 02:03:03 -0400

    EDIT error from image blending

--- qwer/src/main/resources/static/js/ImageEffectHandling.js
+++ qwer/src/main/resources/static/js/ImageEffectHandling.js
@@ -20,16 +20,16 @@ $(document).ready(function(){
 
         console.log(mouseUpObject);
 
-        let img1 =mouseUpObject.target._objects[0].src
-        let img2 =mouseUpObject.target._objects[1].src
+        let img1 =mouseUpObject.target._objects[0]._originalElement
+        let img2 =mouseUpObject.target._objects[1]._originalElement
 
 
         mouseUpObject.target._objects[0]._originalElement.setAttribute('crossOrigin','Anonymous');
         // mouseUpObject.target._objects[0]._element.src = mouseUpObject.target._objects[0]._element.src + '?' + new Date().getTime();
         mouseUpObject.target._objects[1]._originalElement.setAttribute('crossOrigin','Anonymous');
         // mouseUpObject.target._objects[1]._element.src = mouseUpObject.target._objects[0]._element.src + '?' + new Date().getTime();
-        image_set.base = img1;
-        image_set.src = img2;
+        image_set.base = img1.src;
+        image_set.src = img2.src;
 
 
         base_canvas = document.getElementById("base-canvas");
@@ -62,6 +62,25 @@ $(document).ready(function(){
 
         initializeCanvas();
 
+    })
+
+
+    $("#changeImageBtn2").click(function(){
+
+        let sample1 = new Image();
+        let sample2 = new Image();
+
+        var base = document.getElementById("mask-canvas");
+        var image = document.getElementById("result-canvas");
+
+
+        sample1.src = base.toDataURL();
+        sample2.src = image.toDataURL();
+
+        image_set.base = sample2.src;
+        image_set.src =  sample1.src;
+
+        initializeCanvas();
     })
 
 
